9fda211057bf44c58c4e4aa48477fd27a22bc2d1
dolad <adavidoladele@gmail.com>
2022-02-09 19:14:09 +0100

    finalizing set

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default () => ({
     db: {
         username:process.env.TALDEV_PG_USER,
@@ -5,5 +8,8 @@ export default () => ({
         port: process.env.TALDEV_PG_PORT ?  parseFloat(process.env.TALDEV_PG_PORT) : 5432,
         database: process.env.TALDEV_DATABASE,
         dialect: 'postgres'
+    },
+    exchangeRate: {
+        baseUrl: process.env.EXCHANGE_RATE_API
     }
 })
